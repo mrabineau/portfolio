79fc0ab7df5a72f2da8e10cb797fa51a8bc17ec2
Mel Rabineau <melanie.rabineau1@gmail.com>
2017-01-18 10:18:39 -0800

    final commit

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -12,7 +12,7 @@ HomeController.$inject=[]
 
       hm.title = "Functionality and Maximalism";
 
-      hm.about= "Hello! I am a front-end web developer with back-end experience. As a design oriented developer, I value vibrant and playful user interfaces. Born and raised in Los Angeles (with stints in New York and Madrid), I graduated from NYU in 2014 and later the prestigious Web Development Immersive at General Assembly. Subsequently, I have experience working across a number of frameworks such as Rails and the MEAN stack. ";
+      hm.about= "Hello! I am a front-end web developer with back-end experience. As a design oriented developer, I value vibrant and playful user interfaces. Born and raised in Los Angeles (with stints in New York and Madrid), I graduated from NYU in 2014 and later the Web Development Immersive at General Assembly. Subsequently, I have experience working across a number of frameworks such as Rails and the MEAN stack. ";
 
       hm.launchModal = launchModal;
 
